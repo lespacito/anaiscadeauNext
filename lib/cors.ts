75ad07aa5746268829526a886bcef87d63cbef4a
lespacito <30557423+lespacito@users.noreply.github.com>
2024-10-03 22:00:19 +0200

    trust host

--- lib/cors.ts
+++ lib/cors.ts
@@ -1,18 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export function cors(req: NextApiRequest, res: NextApiResponse) {
+export async function cors(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Tu peux restreindre cela à un domaine spécifique
+
+  // Restreindre cela à ton domaine en production pour plus de sécurité
+  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
   );
+
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type, Cache-Control, Pragma"
   );
 
-  // Si c'est une requête préliminaire OPTIONS, répond directement
+  // Si c'est une requête préliminaire OPTIONS, répondre directement
   if (req.method === "OPTIONS") {
     res.status(200).end();
     return;
