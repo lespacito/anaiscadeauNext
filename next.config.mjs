59bca34abb1060775fb66b6df89be05db22c2317
lespacito <30557423+lespacito@users.noreply.github.com>
2024-10-13 15:29:15 +0200

    Resolve som issus

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    images: {
-      domains: ["anaiscadeau.vercel.app"],
-    },
+    domains: ["anaiscadeau.vercel.app"],
   },
 };
 
