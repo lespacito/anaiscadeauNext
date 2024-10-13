54a1b2e60f14b31b8823d1e1e1adb124138aef87
lespacito <30557423+lespacito@users.noreply.github.com>
2024-10-13 15:52:51 +0200

    Resolve som issus

--- components/sections/hero-top.tsx
+++ components/sections/hero-top.tsx
@@ -1,14 +1,11 @@
 import ShinyButton from "@/components/ui/shiny-button";
 import { Spotlight } from "@/components/ui/spotlight";
-import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import imghero from "../../public/assets/img/karting.webp";
 const HeroTop = () => {
   return (
     <>
-      <Head>
-        <link rel="preload" href="/assets/img/karting.webp" as="image" />
-      </Head>
       <div className="max-w-screen-xl mx-auto px-8 py-10">
         {/* Spotlight component for decorative purposes */}
         <Spotlight
@@ -34,12 +31,7 @@ const HeroTop = () => {
 
           {/* Image section */}
           <div className="hidden md:flex md:w-1/2 mt-10 md:mt-0 justify-center">
-            <Image
-              src="/assets/img/karting.webp"
-              alt="image logo"
-              priority
-              quality={75}
-            />
+            <Image src={imghero} alt="image hero" quality={75} priority />
           </div>
         </div>
       </div>
